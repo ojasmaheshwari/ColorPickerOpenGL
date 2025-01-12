68c4486978ef6369f2c847c1b4e661b4fadb94dd
Ojas Maheshwari <ojasmaheshwari2006@gmail.com>
2025-01-12 11:36:57 +0530

    Imgui working check

---
+++ build/CMakeFiles/ColorPicker.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ColorPicker.dir/link.d"
+  "CMakeFiles/ColorPicker.dir/src/imgui/imgui.cpp.o"
+  "CMakeFiles/ColorPicker.dir/src/imgui/imgui.cpp.o.d"
+  "CMakeFiles/ColorPicker.dir/src/imgui/imgui_demo.cpp.o"
+  "CMakeFiles/ColorPicker.dir/src/imgui/imgui_demo.cpp.o.d"
+  "CMakeFiles/ColorPicker.dir/src/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/ColorPicker.dir/src/imgui/imgui_draw.cpp.o.d"
+  "CMakeFiles/ColorPicker.dir/src/imgui/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/ColorPicker.dir/src/imgui/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/ColorPicker.dir/src/imgui/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/ColorPicker.dir/src/imgui/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/ColorPicker.dir/src/imgui/imgui_tables.cpp.o"
+  "CMakeFiles/ColorPicker.dir/src/imgui/imgui_tables.cpp.o.d"
+  "CMakeFiles/ColorPicker.dir/src/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/ColorPicker.dir/src/imgui/imgui_widgets.cpp.o.d"
+  "CMakeFiles/ColorPicker.dir/src/main.cpp.o"
+  "CMakeFiles/ColorPicker.dir/src/main.cpp.o.d"
+  "ColorPicker"
+  "ColorPicker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ColorPicker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
