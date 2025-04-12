1573dfe7ffc6b2052910368958ec60319d263a76
Ojas Maheshwari <ojasmaheshwari2006@gmail.com>
2025-04-12 17:42:06 +0530

    Complete

--- src/Application.cpp
+++ src/Application.cpp
@@ -10,12 +10,70 @@ Application::Application()
 Application::~Application() {}
 
 void Application::run() {
+	unsigned int vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(m_TrianglePositions), m_TrianglePositions, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Position2D), 0);
+
+	std::string vertexShaderSource = loadShaderSourceFromPath("src/shaders/vertex.glsl");
+	std::string fragmentShaderSource = loadShaderSourceFromPath("src/shaders/fragment.glsl");
+
+	m_shaderProgram = CreateShader(vertexShaderSource, fragmentShaderSource);
+	glUseProgram(m_shaderProgram);
+
+	m_ColorLocation = glGetUniformLocation(m_shaderProgram, "u_Color");
+
 	renderLoop();
 }
 
 void Application::initialize() {
 	glfwInitialize();
 	imguiInitialize();
+}
+
+unsigned int Application::CompileShader(GLenum type, const std::string& shaderSource) {
+	unsigned int id = glCreateShader(type);
+	const char* src = shaderSource.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	return id;
+}
+
+unsigned int Application::CreateShader(const std::string &vertexShader, const std::string &fragmentShader) {
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
+
+std::string Application::loadShaderSourceFromPath(const std::string& path) {
+	std::ifstream file(path);
+
+	if (!file.is_open()) {
+		m_Logger.error("Unable to open shader source file");
+		return "INVALID_SHADER_DUE_TO_ERROR";
+	}
+
+	std::string line;
+	std::string source;
+	while (std::getline(file, line)) {
+		source += line + "\n";
+	}
+
+	return source;
 }
 
 void GLAPIENTRY Application::glfwErrorCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam) {
@@ -68,9 +126,9 @@ void Application::glfwInitialize() {
 	}
 
 	// Window hints
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	/*glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);*/
 	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
 
 	m_Window = glfwCreateWindow(640, 480, "Color Picker OpenGL", NULL, NULL);
@@ -89,6 +147,9 @@ void Application::glfwInitialize() {
 
 	glEnable(GL_DEBUG_OUTPUT);
 	glDebugMessageCallback(glfwErrorCallback, nullptr);
+
+	std::string versionStr = "OpenGL Version: " + std::string(glfwGetVersionString());
+	m_Logger.info(versionStr);
 }
 
 void Application::imguiInitialize() {
@@ -108,6 +169,9 @@ void Application::renderLoop() {
 	while (!glfwWindowShouldClose(m_Window)) {
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glUniform4f(m_ColorLocation, m_Color.red, m_Color.green, m_Color.blue, 1.0f);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
 		imguiRender();
 
 		glfwSwapBuffers(m_Window);
@@ -121,8 +185,11 @@ void Application::imguiRender() {
 	ImGui_ImplGlfw_NewFrame();
 	ImGui::NewFrame();
 
-	ImGui::Begin("Sample ImGui Window");
-	ImGui::Text("Hello there!");
+	ImGui::Begin("Color Dialog");
+	ImGui::Text("Color Picker");
+	ImGui::SliderFloat("Red", &m_Color.red, 0.0f, 1.0f);
+	ImGui::SliderFloat("Green", &m_Color.green, 0.0f, 1.0f);
+	ImGui::SliderFloat("Blue", &m_Color.blue, 0.0f, 1.0f);
 	ImGui::End();
 
 	ImGui::Render();
--- src/includes/Application.h
+++ src/includes/Application.h
@@ -7,9 +7,31 @@
 #include <GL/glew.h>
 #include <GL/gl.h>
 #include <GLFW/glfw3.h>
+
 #include <iostream>
 #include <stdio.h>
 #include <Logging.h>
+#include <fstream>
+#include <string>
+
+struct Color {
+	float red;
+	float green;
+	float blue;
+	float alpha;
+
+	Color() : red(0.0f), green(0.0f), blue(0.0f), alpha(0.0f) {}
+	Color(const float red, const float green, const float blue, const float alpha) : red(red), green(green), blue(blue), alpha(alpha) {}
+};
+
+struct Position2D {
+	float x;
+	float y;
+
+	Position2D() : x(0.0f), y(0.0f) {}
+	Position2D(const float x, const float y) : x(x), y(y) {}
+};
+
 
 class Application {
 public: // public methods
@@ -25,8 +47,19 @@ private: // private methods
 	static void GLAPIENTRY glfwErrorCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam);
 	void glfwInitialize();
 	void imguiInitialize();
+	unsigned int CompileShader(GLenum type, const std::string& shaderSource);
+	unsigned int CreateShader(const std::string& vertexShader, const std::string &fragmentShader);
+	std::string loadShaderSourceFromPath(const std::string& path);
 
 private: // private members
 	GLFWwindow* m_Window;
 	Logging m_Logger;
+	Color m_Color;
+	Position2D m_TrianglePositions[3] = {
+		{-0.5f, -0.5f},
+		{0.0f, 0.5f},
+		{0.5f, -0.5f}
+	};
+	GLuint m_shaderProgram;
+	int m_ColorLocation;
 };
